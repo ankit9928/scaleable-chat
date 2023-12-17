1886614be5c23436ba5cdab7ad1398b6184c8891
ankit9928 <ankit_m@ph.iitr.ac.in>
2023-12-17 16:39:02 +0530

    Scalable web-chat

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -10,6 +10,7 @@ interface SocketProviderProps {
 
 interface ISocketContext {
   sendMessage: (msg: string) => any;
+  messages: string[];
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
@@ -24,6 +25,8 @@ export const useSocket = () => {
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   const [socket, setSocket] = useState<Socket>();
 
+  const [messages, setMessages] = useState<string[]>([]);
+
   const sendMessage: ISocketContext["sendMessage"] = useCallback(
     (msg) => {
       console.log("Send Message", msg);
@@ -35,19 +38,28 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
     [socket]
   );
 
+  const onMessageRec = useCallback((msg: string) => {
+    console.log("From server message Rec", msg);
+    const { message } = JSON.parse(msg) as { message: string };
+
+    setMessages((prev) => [...prev, message]);
+  }, []);
+
   useEffect(() => {
     const _socket = io("http://localhost:8000");
+    _socket.on("message", onMessageRec);
 
     setSocket(_socket);
 
     return () => {
       _socket.disconnect();
+      _socket.off("message", onMessageRec);
       setSocket(undefined);
     };
   }, []);
 
   return (
-    <SocketContext.Provider value={{ sendMessage }}>
+    <SocketContext.Provider value={{ sendMessage, messages }}>
       {children}
     </SocketContext.Provider>
   );
